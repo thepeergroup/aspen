fe6d7b150962b8997d12e9ba4ec573e05dfe5b79
Matthew J Cloyd <matt.cloyd@gsa.gov>
2021-09-23 14:08:35 -0400

    Add renderers to encapsulate formatting compiled results

---
+++ lib/aspen/renderers.rb
@@ -0,0 +1,9 @@
+module Aspen
+  module Renderers
+  end
+end
+
+require 'aspen/renderers/abstract_renderer'
+require 'aspen/renderers/cypher_renderer'
+require 'aspen/renderers/gexf_renderer'
+require 'aspen/renderers/json_renderer'
---
+++ lib/aspen/renderers/gexf_renderer.rb
@@ -0,0 +1,47 @@
+module Aspen
+  module Renderers
+    class GexfRenderer < AbstractRenderer
+
+      def render
+        joiner = "\n            "
+        <<~GEXF
+          <gexf xmlns="http://www.gexf.net/1.2draft" version="1.2">
+              <graph mode="static" defaultedgetype="directed">
+                  <nodes>
+                      #{nodes.map(&:strip).join(joiner)}
+                  </nodes>
+                  <edges>
+                      #{relationships.map(&:strip).join(joiner)}
+                  </edges>
+              </graph>
+          </gexf>
+        GEXF
+      end
+
+      def nodes
+        statements.flat_map(&:nodes).map do |node|
+          attrs = node.attributes.map do |k, v|
+            "#{k}=\"#{v}\""
+          end.join(" ")
+          <<~GEXF
+            <node id="#{node.nickname}" label="#{node.label}" #{attrs}>
+          GEXF
+        end
+      end
+
+      def relationships
+        # @todo: Add in Custom Statements
+        statements.map.with_index do |st, id|
+          if st.is_a? Aspen::CustomStatement
+            next # NO OP
+          else
+            <<~GEXF
+              <edge id="#{id}" source="#{st.origin.nickname}" target="#{st.destination.nickname}" label="#{st.edge.label}">
+            GEXF
+          end
+        end.compact
+      end
+
+    end
+  end
+end
